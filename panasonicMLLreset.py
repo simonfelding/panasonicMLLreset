c339901844c17592032d1db0194e58f6d80ac82d
simonfelding <45149055+simonfelding@users.noreply.github.com>
2020-09-26 01:58:38 +0200

    Update panasonicMLLreset.py

--- panasonicMLLreset.py
+++ panasonicMLLreset.py
@@ -32,48 +32,55 @@ from syslog import syslog
 read_range=range(0,10) # why do we even need this extended range?
 write_range=range(1,7)
 
-i2c = SMBus(1)  # Create a new I2C bus
+# i2c = SMBus(1)  # Create a new I2C bus
 dev_addr = int() # blank for now
 
 def find_tv():
 	found = []
 	global dev_addr
-	for arg in sys.argv:
+
+	for arg in sys.argv[1:]:
 		try:
-			dev_addr = int(sys.argv[arg])
+			int(sys.argv[arg+1])
 		except:
 			pass
+		else:
+			dev_addr = int(sys.argv[arg+1])
+
 		## Scan valid I2C 7-bit address range, avoiding invalid addresses:
         ## (0-2=Different bus formats, 120-127=Reserved/10bit addresses)
         ## NOTE: If the SDA line is being held LOW, it will appear that
         ## devices are present at all slave addresses.
-	for addr in range(3, 120):
-        try:
-            ## Taken from i2cdetect.c - Do not perform write operations on
-            ## addresses that might contain EEPROMs to avoid corruption.
-            if addr in range(0x30, 0x38) or addr in range(0x50, 0x60):
-                # Do a 1-byte read to see if a device exists at address.
-                i2c.read_byte(addr, 1)
-        except (IOError) as e:
-            # No ACK. Address is vacant.
-            pass
-        else:
-            # Transaction was ACK'd. Found a device.
-            found.append(addr)
-		if len(found) > 1:
-			print("Found multiple i2c devices, find out which one it is. Here are the addresses: ")
-			print(found)
-			if "-s" not in sys.argv[1:]:
-				print("run without -s to input address manually, or modify the script")
-				dev_addr = int(return("Enter device address (remember prefix 0x for hexadecimal input): "), 0) # ,0 specifies that python can guess if it's an int or a hex string that has been input.
-		elif len(found == 1):
-			dev_addr = found[0])
-			print("dev address is " + str(dev_addr))
+	with SMBus(1) as i2c:
+		for addr in range(3, 120):
+	        try:
+	            ## Taken from i2cdetect.c - Do not perform write operations on
+	            ## addresses that might contain EEPROMs to avoid corruption.
+
+				if addr in range(0x30, 0x38) or addr in range(0x50, 0x60):
+	                # Do a 1-byte read to see if a device exists at address.
+	                	i2c.read_byte(dev_addr, addr)
+	        except (IOError) as e:
+	            # No ACK. Address is vacant.
+	            pass
+	        else:
+	            # Transaction was ACK'd. Found a device.
+	            found.append(addr)
+
+	if len(found) > 1:
+		print("Found multiple i2c devices, find out which one it is. Here are the addresses: ")
+		print(found)
+		if "-s" not in sys.argv[1:]:
+			print("run without -s to input address manually, or modify the script")
+			dev_addr = int(return("Enter device address (remember prefix 0x for hexadecimal input): "), 0) # ,0 specifies that python can guess if it's an int or a hex string that has been input.
+	elif len(found == 1):
+		dev_addr = found[0])
+		print("dev address is " + str(dev_addr))
+	else:
+		if "-s" not in sys.argv[1:]:
+			print("No device found")
 		else:
-			if "-s" not in sys.argv[1:]:
-				print("No device found")
-			else:
-				syslog("No i2c device found when scanning. No writing was attempted.")
+			syslog("No i2c device found when scanning. No writing was attempted.")
 
 def read_data(range):
 	data = ""
